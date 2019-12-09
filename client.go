8d48f8c2da64c1556e065e803303ae9921a3c645
Flavien binet <flavien.binet@gmail.com>
2019-12-09 15:21:37 +0100

    Moved license on top of files. Fix #12

--- client.go
+++ client.go
@@ -1,4 +1,17 @@
+// Copyright 2018-2019-2020 Teserakt AG
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package e4go provides a e4 client implementation and libraries.
 //
 // It aims to be quick and easy to integrate in IoT devices applications
@@ -29,20 +42,6 @@
 // and the client will automatically unprotect and process it (thus returning no unprotected message).
 // See commands.go for the list of available commands and their respective parameters.
 package e4go
-
-// Copyright 2018-2019-2020 Teserakt AG
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 import (
 	"bytes"
