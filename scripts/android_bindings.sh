173b51e1c285b690cce591b4d15c6666884b2c91
Flavien binet <flavien.binet@gmail.com>
2020-03-16 12:12:41 +0100

    Added option to add a version to the generated bindings. Added go.mod cleanup after generation

--- scripts/android_bindings.sh
+++ scripts/android_bindings.sh
@@ -4,6 +4,9 @@
 #     export ANDROID_HOME=~/Android/Sdk/
 #     export ANDROID_NDK_HOME=~/Android/Sdk/ndk/21.0.6113669/
 # (These are the default paths where Android Studio is installing the SDK and NDK, the version might need to be adjusted depending on your setup)
+# A version string can be appended to the output files by specifying a E4VERSION environment variable:
+#     E4VERSION=v1.1.0 ./scripts/android_bindings.sh
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -14,3 +17,11 @@ INCLUDE_GO_PACKAGES=""
 mkdir -p "${OUTDIR}" 2>/dev/null
 
 gomobile bind -v -target android -o "${OUTDIR}/e4.aar" -javapkg io.teserakt ${DIR}/../ ${DIR}/../crypto
+
+if [ ! -z "${E4VERSION}" ]; then
+    mv "${OUTDIR}/e4.aar" "${OUTDIR}/e4_${E4VERSION}.aar"
+    mv "${OUTDIR}/e4-sources.jar" "${OUTDIR}/e4-sources_${E4VERSION}.jar"
+fi
+
+# gomobile will mess up the go.mod file when running, tidying restore it to the appropriate state
+cd "${DIR}/../" && go mod tidy && cd -
